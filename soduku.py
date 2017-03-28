dbdca7bd36f33c16b83827c0fff77cb7d4f419c1
李范红 <vivi3n@126.com>
2017-03-28 21:53:43 +0800

    stay up to date

--- soduku.py
+++ soduku.py
@@ -37,32 +37,27 @@ square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
 
 unitlist = row_units + column_units + square_units
 
-#==============================================================================
-# 
-# #first version of grid, with '.' standing for empty box
-# def grid_values(grid):
-#     """
-#     Convert grid string into {<box>: <value>} dict with '.' value for empties.
-# 
-#     Args:
-#         grid: Sudoku grid in string form, 81 characters long
-#     Returns:
-#         Sudoku grid in dictionary form:
-#         - keys: Box labels, e.g. 'A1'
-#         - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
-#     """
-#     #this is my solution and it passed the test
-#     fill = {}
-#     for i in range(len(boxes)):
-#         fill[boxes[i]] = grid[i]
-#     return fill
-#     #this is udacity's
-#     """
-#     assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
-#     return dict(zip(boxes, grid))
-#     """
-#==============================================================================
+#first version of grid, with '.' standing for empty box
+def grid_values_1(grid):
+    """
+    Convert grid string into {<box>: <value>} dict with '.' value for empties.
 
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    #this is my solution and it passed the test
+    fill = {}
+    for i in range(len(boxes)):
+        fill[boxes[i]] = grid[i]
+    return fill
+    #this is udacity's
+def grid_values_answer(grid):
+    assert len(grid) == 81, "Input grid must be a string of length 81 (9x9)"
+    return dict(zip(boxes, grid))
  
 # Apply the strategy of elimination to fill in the empty boxes
 def grid_values(grid):
@@ -93,7 +88,7 @@ def grid_values(grid):
             fill[boxes[i]] = grid[i]
     return fill
     #this is udacity's solution
-    """
+def grid_values_answers(grid):
     values = []
     all_digits = '123456789'
     for c in grid:
@@ -103,7 +98,6 @@ def grid_values(grid):
             values.append(c)
     assert len(values) == 81
     return dict(zip(boxes, values))
-    """
 
 def eliminate1(values):
     """
@@ -137,7 +131,7 @@ def eliminate1(values):
     assert len(values) == 81
     return values
     #this is udacity's solution
-    """
+def eliminate_answers(values):
     solved_values = [box for box in values.keys() if len(values[box]) == 1] #all the boxes with 1 item 
     #the step above is essential cause actually this is manipulating the dict
     #and if I did't record all the 1 item box at first, as I'm processing there might 
@@ -147,7 +141,6 @@ def eliminate1(values):
         for peer in peers[box]:
             values[peer] = values[peer].replace(digit,'')
     return values
-    """
 
     #in the utils.py we've got two supplimentary dicts
     units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
@@ -188,6 +181,62 @@ def eliminate(values):
                 values[unit_keys] = values[unit_keys].replace(values[key],"")
 #    assert len(values) == 81 #assume value is a standard grid
     return values
+
+#my first and failed try
+def only_choice1(values):
+    """
+    Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    # TODO: Implement only choice strategy here
+    unsolved_values = [box for box in values.keys() if len(values[box]) > 1]
+    for box in unsolved_values:
+        for num in values[box]:  #number char in a str
+            count = 0
+            for peerbox in peers[box]:  #key of peer box 
+            #this is not working cause every unit follows a set of rules 
+            #it's too strict to ask every number not existing in all peerboxs
+                if num in values[peerbox]:
+                    count += 1
+#            print(count)
+            if count == 0:
+                values[box] = num
+    return values
+#this is my second try
+def only_choice(values):
+    for unit in unitlist:
+        for digit in "123456789":
+            count = 0
+            for box in unit:
+                if digit in values[box]:
+                    count += 1
+#                    print(count)
+            if count == 1:                
+                for box in unit:
+                    if digit in values[box]:
+                        values[box] = digit
+    return values
+#this is udacity's code
+def only_choice_answer(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+
+
+out = eliminate(grid_values("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."))
+only_choice(out)
+
+
+
 
 
 
